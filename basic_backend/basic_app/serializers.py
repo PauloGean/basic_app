dcba3ba8be3a507d69275086ffa1829958e0225b
Paulo Lopes <paulogean2@gmail.com>
2024-05-30 18:10:09 -0400

    serializer id

--- basic_backend/basic_app/serializers.py
+++ basic_backend/basic_app/serializers.py
@@ -2,9 +2,9 @@ from rest_framework import serializers
 from . import models
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CustomUser
-        fields = ['url',  'email', 'first_name','last_name', 'password', 'is_active', 'is_superuser']
+        fields = ['id', 'url',  'email', 'first_name','last_name', 'password', 'is_active', 'is_superuser']
 
 
--- basic_backend/basic_app/views.py
+++ basic_backend/basic_app/views.py
@@ -1,6 +1,9 @@
+from django.db.models import Count
 from rest_framework import viewsets
 from basic_app.models import CustomUser
 from basic_app.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -10,3 +13,8 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     # permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=False)
+    def report_user(self, request, **kwargs):
+        self.queryset = CustomUser.objects.all().values('is_superuser', 'is_active').annotate(total=Count('is_active'))
+        return Response(self.queryset)
